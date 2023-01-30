a07ec3debfdab105494970ee385869d1a9a05d5f
leoggehrer <g.gehrer@htl-leonding.ac.at>
2023-01-30 15:56:05 +0100

    Added OnPropertyChanged(...)

--- QuickTemplate.MvvMApp/ViewModels/BaseViewModel.cs
+++ QuickTemplate.MvvMApp/ViewModels/BaseViewModel.cs
@@ -1,12 +1,16 @@
 ﻿//@BaseCode
 //MdStart
 using ReactiveUI;
-using System.ComponentModel;
+using System.Runtime.CompilerServices;
 
 namespace QuickTemplate.MvvMApp.ViewModels
 {
     public class BaseViewModel : ReactiveObject
     {
+        public virtual void OnPropertyChanged([CallerMemberName]string? propertyName = null)
+        {
+            this.RaisePropertyChanged(propertyName);
+        }
     }
 }
 //MdEnd
