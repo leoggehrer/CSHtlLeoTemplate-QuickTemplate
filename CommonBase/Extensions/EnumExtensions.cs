0552619f89f49d7e9d614797722e61046ad18501
Gehrer Gerhard <56598504+leoggehrer@users.noreply.github.com>
2023-04-11 18:22:07 +0200

    Added ToDictionary(...).

--- CommonBase/Extensions/EnumExtensions.cs
+++ CommonBase/Extensions/EnumExtensions.cs
@@ -24,6 +24,12 @@ namespace CommonBase.Extensions
 
             return (values.Length == idx) ? values[0] : values[idx];
         }
+        public static Dictionary<int, string> ToDictionary<T>(this T src) where T : struct, Enum
+        {
+            return Enum.GetValues(typeof(T))
+               .Cast<T>()
+               .ToDictionary(t => (int)(object)t, t => t.ToString());
+        }
     }
 }
 //MdEnd
